a6306b4c191e2fe9fc79d9aab9d0929375d7c4d1
Gobinda Das <dasgobinda9955@gmail.com>
2025-06-12 04:41:43 +0530

    feat: add custom MongoDB plugin using fastify-plugin with Mongoose integration

---
+++ plugins/mongoose.mjs
@@ -0,0 +1,14 @@
+import fastifyPlugin from "fastify-plugin";
+import mongoose from "mongoose";
+
+
+export default fastifyPlugin(async (fastify, opts) => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        fastify.decorate("mongoose", mongoose);
+        fastify.log.info("MONGODB connected !");
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+});
